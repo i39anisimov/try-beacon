c95f5124b564b67c332d173d67b3eeef80df985e
ivn_ <i39anisimov@gmail.com>
2018-07-11 17:54:31 +0300

    low level service

--- app/src/main/java/io/sibur/try_beacon/BeaconsService.kt
+++ app/src/main/java/io/sibur/try_beacon/BeaconsService.kt
@@ -1,73 +1,130 @@
 package io.sibur.try_beacon
 
 import android.app.Service
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.le.*
 import android.content.Intent
 import android.os.Environment
 import android.os.IBinder
-import android.os.RemoteException
 import android.util.Log
-import org.altbeacon.beacon.BeaconConsumer
-import org.altbeacon.beacon.BeaconManager
-import org.altbeacon.beacon.BeaconParser
-import org.altbeacon.beacon.Region
-import org.altbeacon.beacon.service.ArmaRssiFilter
 import java.io.File
 import java.io.FileWriter
 import java.io.IOException
+import kotlin.experimental.and
 
 private const val TAG = "try-beacon"
-private const val IBEACON_LAYOUT = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"
-private const val BETWEEN_SCAN_PERIOD = 0L
 private const val SCAN_PERIOD = 10000L
 
-class BeaconsService : Service(), BeaconConsumer {
+class BeaconsService : Service() {
+    private lateinit var bluetoothAdapter: BluetoothAdapter
+    private lateinit var bluetoothLeScanner: BluetoothLeScanner
+    private val dataList1 = ArrayList<CheckItem>()
+    private val dataList2 = ArrayList<CheckItem>()
+    private val dataList3 = ArrayList<CheckItem>()
 
-    companion object {
-        val region = Region("any", null, null, null)
-    }
+    private var mScanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            val parsedResult = parseData(result.scanRecord.bytes)
+            if (parsedResult.first == 1) {
+                addItem(parsedResult.second, result.rssi, result.timestampNanos / 1000)
+                Log.d(TAG, "${parsedResult.first}/${parsedResult.second}/${result.rssi}")
+            }
+        }
 
-    private lateinit var beaconManager: BeaconManager
-    private val dataList = ArrayList<CheckItem>()
+        override fun onBatchScanResults(results: List<ScanResult>) {
+            for (sr in results) {
+                onScanResult(-1, sr)
+            }
+        }
 
-    override fun onCreate() {
-        super.onCreate()
-        init()
+        override fun onScanFailed(errorCode: Int) {
+            Log.e(TAG, "BLE// onScanFailed. Error Code: $errorCode")
+        }
+
+
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        beaconManager.bind(this)
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
 
+        val settings = ScanSettings.Builder()
+            .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+            .build()
+
+        val filters = ArrayList<ScanFilter>()
+
+        bluetoothLeScanner.startScan(filters, settings, mScanCallback)
         return START_NOT_STICKY
     }
 
     override fun onDestroy() {
-        beaconManager.stopRangingBeaconsInRegion(BeaconsService.region)
-        beaconManager.unbind(this)
-
-        saveToCSV()
-        dataList.clear()
+        bluetoothLeScanner.stopScan(mScanCallback)
+        saveToCSV(dataList1)
+        saveToCSV(dataList2)
+        saveToCSV(dataList3)
 
         super.onDestroy()
     }
 
-    override fun onBeaconServiceConnect() {
-        beaconManager.addRangeNotifier { beacons, _ ->
-            addItem(CheckItem.create(beacons))
+    private fun parseData(scanRecord: ByteArray): Pair<Int, Int> {
+        var startByte = 2
+        var patternFound = false
+        var major = 0
+        var minor = 0
+        while (startByte <= 5) {
+            if (scanRecord[startByte + 2] and 0xff.toByte() == 0x02.toByte() &&
+                scanRecord[startByte + 3] and 0xff.toByte() == 0x15.toByte()) {
+                patternFound = true
+                break
+            }
+            startByte++
         }
+
+        if (patternFound) {
+            val uuidBytes = ByteArray(16)
+            System.arraycopy(scanRecord, startByte + 4, uuidBytes, 0, 16)
+            val hexString = bytesToHex(uuidBytes)
+
+            val uuid = hexString.substring(0, 8) + "-" +
+                hexString.substring(8, 12) + "-" +
+                hexString.substring(12, 16) + "-" +
+                hexString.substring(16, 20) + "-" +
+                hexString.substring(20, 32)
+
+            major = (scanRecord[startByte + 20] and 0xff.toByte()) * 0x100 + (scanRecord[startByte + 21] and 0xff.toByte())
+            minor = (scanRecord[startByte + 22] and 0xff.toByte()) * 0x100 + (scanRecord[startByte + 23] and 0xff.toByte())
+        }
+        return Pair(major, minor)
+    }
+
+    private fun bytesToHex(bytes: ByteArray): String {
+        val hexArray = "0123456789ABCDEF".toCharArray()
+        val hexChars = CharArray(bytes.size * 2)
         try {
-            beaconManager.startRangingBeaconsInRegion(BeaconsService.region)
-        } catch (e: RemoteException) {
-            e.printStackTrace()
+            for (j in bytes.indices) {
+                val v = bytes[j] and 0xFF.toByte()
+                hexChars[j * 2] = hexArray[v.toInt().ushr(4)]
+                hexChars[j * 2 + 1] = hexArray[v.toInt() and 0x0F]
+            }
+        } catch (ex: Exception) {
+//            ex.printStackTrace()
         }
+        return String(hexChars)
     }
 
-    private fun addItem(item: CheckItem) {
-        dataList.add(item)
+    private fun addItem(minor: Int, rssi: Int, timeStamp: Long) {
+        when(minor) {
+            2 -> dataList1
+            8 -> dataList2
+            9 -> dataList3
+            else -> null
+        }?.add(CheckItem(rssi, timeStamp))
     }
 
-    private fun saveToCSV() {
+    private fun saveToCSV(list: ArrayList<CheckItem>) {
         var fileWriter: FileWriter? = null
-        val CSV_HEADER = "n,1,2,3,4"
+        val CSV_HEADER = "time,rssi"
 
         try {
             val file = File(Environment.getExternalStorageDirectory(), "download/" + System.currentTimeMillis().toString() + ".txt")
@@ -77,12 +134,12 @@ class BeaconsService : Service(), BeaconConsumer {
             fileWriter.append(CSV_HEADER)
             fileWriter.append('\n')
 
-            var num = 1
-            for (check in dataList) {
-                fileWriter.append("$num")
-                check.data.map { fileWriter!!.append(", $it") }
-                fileWriter.append('\n')
-                num++
+//            var num = 1
+            for (check in list) {
+                fileWriter.append("${check.timestamp},${check.rssi}\n")
+//                check.data.map { fileWriter!!.append(", $it") }
+//                fileWriter.append('\n')
+//                num++
             }
 
             Log.d(TAG, "Write CSV successfully!")
@@ -98,17 +155,6 @@ class BeaconsService : Service(), BeaconConsumer {
                 e.printStackTrace()
             }
         }
-    }
-
-    private fun init() {
-        beaconManager = BeaconManager.getInstanceForApplication(this)
-        beaconManager.beaconParsers.add(BeaconParser().setBeaconLayout(IBEACON_LAYOUT))
-        BeaconManager.setRssiFilterImplClass(ArmaRssiFilter::class.java)
-        BeaconManager.setAndroidLScanningDisabled(true)
-        beaconManager.backgroundBetweenScanPeriod = BETWEEN_SCAN_PERIOD
-        beaconManager.backgroundScanPeriod = SCAN_PERIOD
-        beaconManager.foregroundBetweenScanPeriod = BETWEEN_SCAN_PERIOD
-        beaconManager.foregroundScanPeriod = SCAN_PERIOD
     }
 
     override fun onBind(intent: Intent): IBinder? {
--- app/src/main/java/io/sibur/try_beacon/CheckItem.kt
+++ app/src/main/java/io/sibur/try_beacon/CheckItem.kt
@@ -1,25 +1,25 @@
 package io.sibur.try_beacon
 
 import org.altbeacon.beacon.Beacon
+import java.sql.Timestamp
 
 interface Factory<out T> {
     fun create(beacons: MutableCollection<Beacon>): T
 }
 
-class CheckItem {
-    val data = IntArray(3, { -100 })
+class CheckItem(var rssi: Int, var timestamp: Long) {
 
-    companion object : Factory<CheckItem> {
-        override fun create(beacons: MutableCollection<Beacon>): CheckItem {
-            val item = CheckItem()
-            beacons.map {
-                when (it.id3.toInt()) {
-                    2 -> item.data[0] = it.rssi
-                    8 -> item.data[1] = it.rssi
-                    9 -> item.data[2] = it.rssi
-                }
-            }
-            return item
-        }
-    }
+//    companion object : Factory<CheckItem> {
+//        override fun create(beacons: MutableCollection<Beacon>): CheckItem {
+//            val item = CheckItem()
+//            beacons.map {
+//                when (it.id3.toInt()) {
+//                    2 -> item.data[0] = it.rssi
+//                    8 -> item.data[1] = it.rssi
+//                    9 -> item.data[2] = it.rssi
+//                }
+//            }
+//            return item
+//        }
+//    }
 }
